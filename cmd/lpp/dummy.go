507ac933ff7c1605c2075e157d580be63e8a3eb7
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2019-07-30 14:57:03 +0200

    Preimage and Hashed Preimage is now stored in transaction records

--- cmd/lpp/dummy.go
+++ cmd/lpp/dummy.go
@@ -26,17 +26,14 @@ func FillWithDummyData(d *sqlx.DB) error {
 
 		transactionCount := gofakeit.Number(1, 20)
 
-		statusOptions := []string{"completed", "failed"}
 		directionOptions := []string{"inbound", "outbound"}
 		for index := 1; index <= transactionCount; index++ {
 			_, err = transactions.CreateInvoice(d, transactions.NewTransaction{
 				UserID:      user.ID,
-				Invoice:     "sadfsdafasdfasdfsdfsadf12321213123",
-				Status:      gofakeit.RandString(statusOptions),
-				Description: "Dummy data",
+				Description: "Dummy data " + string(index),
 				Direction: transactions.Direction(
 					gofakeit.RandString(directionOptions)),
-				Amount: int64(gofakeit.Number(50, 4000000)),
+				Amount: int64(gofakeit.Number(50, 10000)),
 			})
 			if err != nil {
 				return err
