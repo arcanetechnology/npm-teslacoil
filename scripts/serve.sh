b9f6f7319eebd54bbd06e3da0d3d284e6114b90c
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2019-10-09 11:44:56 +0200

    Added missing serve.sh env variables

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -11,6 +11,20 @@ if [[ -z "$ZMQPUBRAWTX_PORT" ]]; then
     export ZMQPUBRAWTX_PORT=23473
 fi
 
+if [[ -z "$RPCUSER" ]]; then
+    export RPCUSER=devuser
+fi
+
+if [[ -z "$RPCPASS" ]]; then
+    export RPCPASS=devpass
+fi
+
+if [[ -z "$LND_DIR" ]]; then
+    export LND_DIR=docker/.alice
+fi
+
+
+
 #function to display and execute commands
 exe() { echo "\$ $@" ; "$@" ; }
 
@@ -19,6 +33,7 @@ exe docker-compose up -d
 
 BITCOIND_IP=`docker inspect --format '{{json .NetworkSettings}}' bitcoind | jq .Networks.teslacoil_default.IPAddress --raw-output`
 exe ./lpp-dev \
+    --lnddir $LND_DIR \
     --bitcoind.rpcuser $RPCUSER \
     --bitcoind.rpcpassword $RPCPASS \
     --bitcoind.rpchost $BITCOIND_IP \
