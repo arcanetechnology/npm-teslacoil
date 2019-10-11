7d58463703ecab1f69efaa9ea1dd68700d6838d9
Torkel Rogstad <torkel@rogstad.io>
2019-10-11 15:33:48 +0200

    Test that standard API response shapes are verified
    In this commit introduce tests that test httptesttutil... We verify that
    all responses that succeed lack the `error` key, and that all responses
    that error follow the standardized error format.

---
+++ testutil/httptestutil/httptestutil_test.go
@@ -0,0 +1,173 @@
+package httptestutil
+
+import (
+	"net/http"
+	"testing"
+
+	"gitlab.com/arcanecrypto/teslacoil/testutil"
+)
+
+type badJson struct{}
+
+func (s badJson) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	if _, err := response.Write([]byte(`-----`)); err != nil {
+		panic(err)
+	}
+}
+
+type goodObject struct{}
+
+func (s goodObject) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	if _, err := response.Write([]byte(`{
+		"foo": "bar"
+	}`)); err != nil {
+		panic(err)
+	}
+}
+
+type goodList struct{}
+
+func (s goodList) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	if _, err := response.Write([]byte(`[]`)); err != nil {
+		panic(err)
+	}
+}
+
+type badServer struct{}
+
+func (s badServer) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	if _, err := response.Write([]byte(`{
+		"error": "bar"
+	}`)); err != nil {
+		panic(err)
+	}
+}
+
+func TestTestHarness_AssertResponseOkWithJson(t *testing.T) {
+	t.Run("accept a normal JSON body", func(t *testing.T) {
+		server := goodObject{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		h.AssertResponseOkWithJson(t, req)
+	})
+
+	t.Run(`fail with a JSON body containing "error"`, func(t *testing.T) {
+		server := badServer{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		testThatShouldFail := testing.T{}
+		h.AssertResponseOkWithJson(&testThatShouldFail, req)
+		testutil.AssertMsg(t, testThatShouldFail.Failed(), "Test didn't fail with bad response")
+	})
+
+	t.Run(`fail with invalid JSON`, func(t *testing.T) {
+		server := badJson{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		testThatShouldFail := testing.T{}
+		h.AssertResponseOkWithJson(&testThatShouldFail, req)
+		testutil.AssertMsg(t, testThatShouldFail.Failed(), "Test didn't fail with bad response")
+	})
+}
+
+func TestTestHarness_AssertResponseOkWithJsonList(t *testing.T) {
+	t.Run("accept a normal JSON body", func(t *testing.T) {
+		server := goodList{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		h.AssertResponseOkWithJsonList(t, req)
+	})
+
+	t.Run(`fail with invalid JSON`, func(t *testing.T) {
+		server := badJson{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		testThatShouldFail := testing.T{}
+		h.AssertResponseOkWithJsonList(&testThatShouldFail, req)
+		testutil.AssertMsg(t, testThatShouldFail.Failed(), "Test didn't fail with bad response")
+	})
+}
+
+type badError struct{}
+
+func (s badError) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusUnauthorized)
+	if _, err := response.Write([]byte(`{
+		"there_should": "be stuff here"
+	}`)); err != nil {
+		panic(err)
+	}
+}
+
+type goodError struct{}
+
+func (s goodError) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusUnauthorized)
+	if _, err := response.Write([]byte(`{ "error": {
+		"message": "this is an error message",
+		"code": "ERR_WITH_A_CODE",
+		"fields": [
+			{
+				"code": "ERR_FIELD_CODE",
+				"message": "field error",
+				"field": "foo-field"
+			}
+		]
+	}}`)); err != nil {
+		panic(err)
+	}
+}
+
+func TestTestHarness_AssertResponseNotOk(t *testing.T) {
+	t.Run("accept a good error response", func(t *testing.T) {
+		server := goodError{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		_, _ = h.AssertResponseNotOk(t, req)
+	})
+
+	t.Run("fail with a 200 code", func(t *testing.T) {
+		server := goodObject{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		testThatShouldFail := testing.T{}
+		res, _ := h.AssertResponseNotOk(&testThatShouldFail, req)
+		testutil.AssertMsg(t, res.Code == 200, "Code wasn't 200")
+		testutil.AssertMsg(t, testThatShouldFail.Failed(), "test didn't fail with 200 code")
+	})
+
+	t.Run("fail with a error code that doesn't have a correct error response", func(t *testing.T) {
+		server := badError{}
+		h := NewTestHarness(server)
+		req := GetRequest(t, RequestArgs{
+			Path:   "/ping",
+			Method: "GET",
+		})
+		testThatShouldFail := testing.T{}
+		res, _ := h.AssertResponseNotOk(&testThatShouldFail, req)
+		testutil.AssertMsg(t, res.Code != 200, "Code was 200")
+		testutil.AssertMsg(t, testThatShouldFail.Failed(), "test didn't fail with bad error message")
+
+	})
+}
