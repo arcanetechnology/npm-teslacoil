84231aa1a8b625c00c0ea9fc22e7c4c33a963e78
Bjorn Olav Jalborg <bojalborg@gmail.com>
2019-10-15 15:35:39 +0200

    add testnet.teslacoil.io to allowed origins

--- scripts/deployTestnet.sh
+++ scripts/deployTestnet.sh
@@ -6,7 +6,5 @@ exe lpp db up
 exe docker-compose up -d bitcoind
 BITCOIND_IP=`docker inspect --format '{{json .NetworkSettings}}' bitcoind | jq .Networks.teslacoil_default.IPAddress --raw-output`
 PREV_IP=`systemctl | grep -m1 teslacoil@ | grep -o -P '(?<=teslacoil).*(?=.service)'`
-exe echo $PREV_IP
-exe echo $BITCOIND_IP
-exe sudo systemctl stop teslacoil@${PREV_IP}.service
+exe sudo systemctl stop teslacoil${PREV_IP}.service
 exe sudo systemctl start teslacoil@"${BITCOIND_IP}".service
