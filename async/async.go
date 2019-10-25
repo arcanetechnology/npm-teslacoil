24128fad6dc7f48b756e801be41ca9ba4138e5bd
Torkel Rogstad <torkel@rogstad.io>
2019-10-25 16:12:29 +0200

    Add new async util for using timeout on waitgroup

--- async/async.go
+++ async/async.go
@@ -4,10 +4,29 @@ package async
 import (
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 
 	pkgerrors "github.com/pkg/errors"
 )
+
+
+// WaitTimeout waits for the waitgroup for the specified max timeout.
+// Returns true if waiting timed out.
+// Cribbed from https://stackoverflow.com/a/32843750/10359642
+func WaitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return false // completed normally
+	case <-time.After(timeout):
+		return true // timed out
+	}
+}
 
 // Retry retries the given function until it doesn't fail. It doubles the
 // period between attempts each time.
@@ -52,11 +71,19 @@ func innerRetryNoBackoff(attempts int, sleep time.Duration, fn func() error) err
 	if err := fn(); err != nil {
 		if attempts > 1 {
 			time.Sleep(sleep)
-			return innerRetry(attempts-1, sleep, fn)
+			return innerRetryNoBackoff(attempts-1, sleep, fn)
 		}
 		return err
 	}
 	return nil
+}
+
+// AwaitNoBackoff attempts the given condition the specified amount of times,
+// sleeping between each attempt. If the condition doesn't succeed,
+// it returns an error saying how many times we tried and how much time it
+// took altogether.
+func AwaitNoBackoff(attempts int, sleep time.Duration, fn func() bool, msgs ...string) error {
+	return await(attempts, sleep, fn, innerAwaitNoBackoff, msgs...)
 }
 
 // Await attempts the given condition the specified amount of times, doubling
@@ -64,8 +91,12 @@ func innerRetryNoBackoff(attempts int, sleep time.Duration, fn func() error) err
 // it returns an error saying how many times we tried and how much time it
 // took altogether.
 func Await(attempts int, sleep time.Duration, fn func() bool, msgs ...string) error {
+	return await(attempts, sleep, fn, innerAwait, msgs...)
+}
+
+func await(attempts int, sleep time.Duration, fn func() bool, waiter func(int, time.Duration, func() bool) bool, msgs ...string) error {
 	start := time.Now()
-	if !innerAwait(attempts, sleep, fn) {
+	if !waiter(attempts, sleep, fn) {
 		end := time.Now()
 		msg := fmt.Sprintf("Condition was not true after %d attempts and %s total waiting time",
 			attempts, end.Sub(start))
@@ -80,8 +111,18 @@ func Await(attempts int, sleep time.Duration, fn func() bool, msgs ...string) er
 	return nil
 }
 
-func innerAwait(attempts int, sleep time.Duration,
-	fn func() bool) bool {
+func innerAwaitNoBackoff(attempts int, sleep time.Duration, fn func() bool) bool {
+	if !fn() {
+		if attempts > 1 {
+			time.Sleep(sleep)
+			return innerAwaitNoBackoff(attempts-1, 2, fn)
+		}
+		return false
+	}
+	return true
+}
+
+func innerAwait(attempts int, sleep time.Duration, fn func() bool) bool {
 	if !fn() {
 		if attempts > 1 {
 			time.Sleep(sleep)
