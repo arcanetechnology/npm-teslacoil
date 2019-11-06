ceec6122fa3a72f4822f2da65d0859af29178d2c
Bjorn Olav Jalborg <bojalborg@gmail.com>
2019-11-06 14:07:58 +0100

    complete necessary functions for lightningspin

--- src/teslacoil.ts
+++ src/teslacoil.ts
@@ -1,72 +1,72 @@
-import axios from 'axios';
+import axios from "axios";
 const api = axios.create();
 
-type environments = "MAINNET" | "TESTNET"
+const apiKeyNotSetMessage = "looks like you haven't set your api-key! set api-key by calling setCredentials(key)";
+type environments = "MAINNET" | "TESTNET";
 let apiKey = "";
 
-
-export const setCredentials = (key = '', network: environments = 'MAINNET') => {
+export const setCredentials = (key = "", network: environments = "MAINNET") => {
   apiKey = key;
-  api.defaults.baseURL = (network === 'MAINNET') ? 'https://api.teslacoil.io' : 'https://testnetapi.teslacoil.io';
+  api.defaults.baseURL = network === "MAINNET" ? "https://api.teslacoil.io" : "https://testnetapi.teslacoil.io";
   api.defaults.timeout = 5000;
-  api.defaults.headers = { 'Authorization' : apiKey, };
-}
+  api.defaults.headers = { Authorization: apiKey };
+};
 
-// transactions
-export const getAllTransactions = async () => {
-  try {
-    const response = await api.get('/transactions');
-    return response.data.data;
-  } catch (error) {
-    throw Error(error);
+export const getByPaymentRequest = async (paymentRequest: string) => {
+  if (apiKey == "") {
+    throw Error(apiKeyNotSetMessage);
   }
-}
-
-export const getTransactionByPaymentRequest = async (paymentRequest: string) => {
   try {
     const response = await api.get(`/invoices/${paymentRequest}`);
-    return response.data.data;
+    return response.data;
   } catch (error) {
     throw Error(error);
   }
-}
+};
+
+export const decodePaymentRequest = async (paymentRequest: string) => {
+  try {
+    const response = await api.get(`/invoices/${paymentRequest}`);
+    return response.data;
+  } catch (err) {
+    throw Error(err);
+  }
+};
 
 interface CreateInvoiceArgs {
   amountSat: number; // must be greater than 0 and less than 4294968
-  memo: string;
-  description: string;
-  callbackUrl: string;
-  orderId: string;
+  memo?: string;
+  description?: string;
+  callbackUrl?: string;
+  orderId?: string;
 }
 
 export const createInvoice = async (args: CreateInvoiceArgs) => {
+  if (apiKey == "") {
+    throw Error(apiKeyNotSetMessage);
+  }
+
   try {
-    const response = await api.post('/invoices/create', args);
-    return response.data.data;
+    const response = await api.post("/invoices/create", args);
+    return response.data;
   } catch (error) {
     throw Error(error);
   }
-}
+};
 
 interface PayInvoiceArgs {
-  paymentRequest: string
-  description: string
+  paymentRequest: string;
+  description?: string;
 }
 
 const payInvoice = async (args: PayInvoiceArgs) => {
+  if (apiKey == "") {
+    throw Error(apiKeyNotSetMessage);
+  }
   try {
-    const response = await api.post('/invoices/pay', args);
-    return response.data.data;
+    const response = await api.post("/invoices/pay", args);
+    return response.data;
   } catch (error) {
     throw Error(error);
   }
-}
-
-/**
- * 
- * @param charge 
-async function verifySignature(charge) {
-  const hash = crypto.createHmac('sha256', api_key).update(charge.id).digest('hex');
-  return hash === charge.hashed_order;
-}
- */
+};
