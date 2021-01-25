49a60e5d391b3b1dd8b7eb1e77ef675bdb7df168
jalborg <bo@jalborg.com>
2021-01-25 12:02:36 +0100

    v0.6.37-6

--- gen-code.sh
+++ gen-code.sh
@@ -8,35 +8,4 @@ node node_modules/restful-react/dist/bin/restful-react.js import --file teslacoi
 
 echo -e "/* eslint-disable */
 
-import axios from \"axios\";
-
-const api = axios.create({
-  validateStatus: () => true,
-});
-
-const apiKeyNotSetMessage = \"looks like you haven't set your api-key! set api-key by calling setCredentials(key)\";
-type environments = \"MAINNET\" | \"TESTNET\" | \"REGTEST\";
-let apiKey = \"\";
-
-export const setCredentials = (key: string, network: environments = \"REGTEST\"): void => {
-  if (key === \"\") {
-    throw Error(\"api key can not be set to empty string\");
-  }
-
-  apiKey = key;
-  switch (network) {
-    case \"MAINNET\":
-      api.defaults.baseURL = \"https://api.teslacoil.io\";
-      break;
-    case \"TESTNET\":
-      api.defaults.baseURL = \"https://testnetapi.teslacoil.io\";
-      break;
-    case \"REGTEST\":
-      api.defaults.baseURL = \"http://localhost:5000\";
-      break;
-  }
-  api.defaults.timeout = 5000;
-  api.defaults.headers = { Authorization: apiKey };
-};
-
 $(cat $DEST_FILE)" > $DEST_FILE
